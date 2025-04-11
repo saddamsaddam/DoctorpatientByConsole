0e9adf0390ad4982a230b5e3cd9eba508fda1cf2
saddamsaddam <56682452+saddamsaddam@users.noreply.github.com>
2025-04-11 10:47:50 +0600

    Add files via upload

---
+++ SeesPatients.java
@@ -0,0 +1,7 @@
+public interface SeesPatients {
+	static final int MAX_PATIENTS = 100;
+	void addPatient(Patient p) throws PatientException;
+	Patient [] getPatients();
+	String getPatientsAsString();
+	boolean isPatient(Patient p);
+}
